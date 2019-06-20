69c82b74f80bc9e85e40a81692378c3d4d2c6806
alamnr <alamnr@gmail.com>
2019-06-20 14:32:24 +0600

    Initial commit

---
+++ src/main/java/io/javabrains/MathUtils.java
@@ -0,0 +1,12 @@
+/**
+ * @author DELL
+ */
+
+package io.javabrains;
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,15 @@
+package io.javabrains;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test(){
+        //fail("Not yet implemented");
+        System.out.println("Your Test Run");
+    }
+
+}
