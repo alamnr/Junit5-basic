70919bc2d73a99963e1fecad13283649b6c8abdb
alamnr <alamnr@gmail.com>
2019-06-21 11:07:34 +0600

    Asserting exception with assert throws

--- src/main/java/io/javabrains/MathUtils.java
+++ src/main/java/io/javabrains/MathUtils.java
@@ -9,4 +9,20 @@ public class MathUtils {
     public int add(int a, int b){
         return a+b;
     }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+
+    public double computeCircleArea(double radious){
+        return Math.PI * radious * radious ;
+    }
 }
--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -5,11 +5,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
+    @Test
+    void subtract() {
+    }
 
     @Test
-    void test(){
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+        MathUtils mathUtils  = new MathUtils();
+        //assertThrows(ArithmeticException.class,()->mathUtils.divide(2,0),"Divide");
+        assertThrows(NullPointerException.class,()->mathUtils.divide(2,0),"Divide");
+    }
+
+    @Test
+    void add() {
         //fail("Not yet implemented");
-        System.out.println("Your Test Run");
+        //System.out.println("Your Test Run");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+
+        assertEquals(expected,actual, "add method");
+    }
+
+    @Test
+    void computeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793,mathUtils.computeCircleArea(10));
     }
 
 }
