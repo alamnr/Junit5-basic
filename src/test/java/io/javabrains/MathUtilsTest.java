db47faf985d8d51bd346f98b63d3bac1f74101aa
alamnr <alamnr@gmail.com>
2019-06-21 12:35:12 +0600

    Examining before all after  all

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,10 +1,33 @@
 package io.javabrains;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
+
+    MathUtils mathUtils;
+
+    @BeforeAll
+    static void beforeAllInit(){
+        System.out.println("This need to run before all  ... ");
+    }
+
+    @AfterAll
+    static void AfterAll(){
+        System.out.println("This need to run after all  ... ");
+    }
+
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanUp(){
+        System.out.println("Cleaning up...");
+    }
+
     @Test
     void subtract() {
     }
@@ -15,16 +38,14 @@ class MathUtilsTest {
 
     @Test
     void divide() {
-        MathUtils mathUtils  = new MathUtils();
-        //assertThrows(ArithmeticException.class,()->mathUtils.divide(2,0),"Divide");
-        assertThrows(NullPointerException.class,()->mathUtils.divide(2,0),"Divide");
+        assertThrows(ArithmeticException.class,()->mathUtils.divide(2,0),"Divide");
+        //assertThrows(NullPointerException.class,()->mathUtils.divide(2,0),"Divide");
     }
 
     @Test
     void add() {
         //fail("Not yet implemented");
         //System.out.println("Your Test Run");
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1,1);
 
@@ -33,7 +54,6 @@ class MathUtilsTest {
 
     @Test
     void computeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793,mathUtils.computeCircleArea(10));
     }
 
